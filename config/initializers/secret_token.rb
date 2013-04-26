5f35b1cba0d0fef9791b861ab2853fc9b8b5d4d3
Jenia Nemzer <jenian@gmail.com>
2013-04-26 14:18:00 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triviamania::Application.config.secret_token = '2e56f3d1cf1dd9232c1c483b46db3f7f911ca03d9fa483d65fd244b30f2cb81f4f91f56f40291d4532ee1baee19a1e200f63d8c83cf5d7066d8fa7074425488b'
