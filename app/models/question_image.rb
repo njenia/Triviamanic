d94e6d483e7e2b8635ae4ee356fda5655edf42c0
Evgeny Nemzer <jenian@gmail.com>
2013-09-16 00:02:24 +0300

    updated application.rb and production.rb for rails 4 compilance

--- app/models/question_image.rb
+++
@@ -1,7 +0,0 @@
-class QuestionImage < ActiveRecord::Base
-  attr_accessible :question_id, :image
-
-  belongs_to :question_image
-
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-end
--- db/migrate/20130504000531_create_question_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestionImages < ActiveRecord::Migration
-  def change
-    create_table :question_images do |t|
-      t.integer :question_id
-
-      t.timestamps
-    end
-  end
-end
